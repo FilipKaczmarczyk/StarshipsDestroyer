0593ba3f061fd61eea4ae59263eacd394688473b
Tomasz Nocoń <tomasz13nocon@gmail.com>
2020-01-18 21:05:40 +0100

    Menu progress and sounds

---
+++ Assets/ButtonSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSound : MonoBehaviour
+{
+
+    public AudioSource audio;
+    public AudioClip hover;
+
+    public void HoverSound()
+    {
+        audio.PlayOneShot(hover);
+    }
+}
