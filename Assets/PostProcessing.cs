f9c114ce65054b2937be421b50695a30a3feaf2e
HappyArroW <stifmeister0700@gmail.com>
2020-01-19 17:23:34 +0100

    postprocessing

---
+++ Assets/PostProcessing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostProcessing : MonoBehaviour
+{
+    public PostProcessVolume volume;
+
+    private Bloom bloom;
+
+    void Start()
+    {
+        volume.profile.TryGetSettings(out bloom);
+
+        bloom.color.value = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bloom.color.value = Color.Lerp(Color.green, Color.red, Mathf.PingPong(Time.time, 1));
+    }
+}
